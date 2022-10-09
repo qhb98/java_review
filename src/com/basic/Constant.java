da7cd30b60c8fab4537bc81520a2c459cbb7958f
qhb98 <2651311819@qq.com>
2022-10-09 15:45:08 +0800

    init

---
+++ src/com/basic/Constant.java
@@ -0,0 +1,28 @@
+package com.basic;
+
+/**
+ * @Description: 常量
+ * @Author: QHB
+ * @Date: 2022/10/9 15:17
+ */
+public class Constant {
+    public static void main(String[] args) {
+        // 使用关键字 final 来定义一个常量, 常量一旦被初始化后就不能再更改
+        final double PI = 3.14;
+        final MAX_SPEED = 120;
+        // 常量的命名全用大写字母 + 下划线_
+
+        // 字符常量  和 符号常量
+
+
+    }
+}
+/*
+变量和常量的命名规范
+1. 所有变量 方法 类名都要符合见名知义的原则
+2. 类成员变量: 首字母小写和驼峰原则 monthSalary
+3. 局部变量: 首字母小写和驼峰原则
+4. 常量: 大写字母和下划线  MAX_VALUE
+5. 类名: 首字母大写和驼峰原则  GoodMan
+6. 方法名: 首字母小写和驼峰原则 runRun()
+ */
---
+++ src/com/basic/DataType.java
@@ -0,0 +1,31 @@
+package com.basic;
+
+/**
+ * @Description: 基本数据类型 primitive data type
+ * @Author: QHB
+ * @Date: 2022/10/9 15:31
+ */
+public class DataType {
+
+
+
+}
+/*
+基本数据类型: 数值型(整数类型 byte, short, int, long; 浮点型 float, double)、字符型(char)、布尔型(boolean)
+引用数据类型: 类class、接口interface、数组array
+
+整型变量和整型常量 --
+    byte  占用 1 bit  表数范围 -2^7 ~ 2^7-1
+    short 占用 2 bit  表数范围 -2^15 ~ 2^15-1
+    int 占用 4 bit
+    long 占用 8 bit
+
+    十进制整数, 如 99 -500
+    八进制整数, 要求以0开头, 如 015
+    十六进制整数, 要求以0x或0X开头, 如 0x15
+    二进制数, 要求0b或0B开头, 如 0b01110011
+
+
+
+
+ */
